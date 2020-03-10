4f2d8c302a4636a6a0ad1fb2fab9592fed69c415
Guo Wanqi <guowanqi@Guos-MacBook-Pro.local>
2020-03-10 21:45:49 +0800

    add login

--- src/App.js
+++ src/App.js
@@ -4,33 +4,49 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
+import amber from "@material-ui/core/colors/amber";
+import { loginUrl } from "./constants/constant";
 
 const useStyles = makeStyles(theme => ({
     title: {
-        marginRight: theme.spacing(5),
         textDecoration: "none",
         color: "black"
     },
+    grow: {
+        flexGrow: 1
+    },
     container: {
         margin: theme.spacing(7)
+    },
+    button: {
+        marginRight: theme.spacing(5),
+        "&:hover": {
+            backgroundColor: amber[300]
+        }
     }
 }));
 
 const App = ({ children }) => {
     const classes = useStyles();
+
+    const login = () => {
+        window.location.href = loginUrl;
+    };
+
     return (
         <>
             <AppBar position="static" color="transparent">
                 <Toolbar>
-                    <Button>
+                    <div className={classes.grow}>
                         <Link to="/" className={classes.title}>
-                            Home
+                            <Button className={classes.button}>Home</Button>
                         </Link>
-                    </Button>
-                    <Button>
                         <Link to="/upload" className={classes.title}>
-                            Upload
+                            <Button className={classes.button}>Upload</Button>
                         </Link>
+                    </div>
+                    <Button onClick={login} className={classes.button}>
+                        Login
                     </Button>
                 </Toolbar>
             </AppBar>
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,6 @@
 const server = "https://fierce-lake-35299.herokuapp.com/";
 const serverUpload = `${server}upload`;
 const videoServer = "https://video-server-sg.s3-ap-southeast-1.amazonaws.com/";
+const loginUrl = `${server}login`;
 
-export { server, serverUpload, videoServer };
+export { server, serverUpload, videoServer, loginUrl };
